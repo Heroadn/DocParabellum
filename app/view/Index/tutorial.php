1910b2e780166030cf01a232a632a000b6d85db3
Benjamin de Castro Azevedo Ponciano <benbenjamin554@gmail.com>
2019-06-16 14:24:10 -0300

    Update tutorial.php

--- app/view/Index/tutorial.php
+++ app/view/Index/tutorial.php
@@ -80,9 +80,9 @@ class <span class="blue">nome</span>Controller extends </span><span class="blue"
                 {
                     //Os atribuos em azul serão passados para a view como variaveis
                     //<span class="blue">'variavel'</span>=>'valor da variavel'
-                    <span class="blue">$this-></span>view('Listar',[<span class="blue">'id'</span> =>$id, <span class="blue">'name'</span>=>$name,<span class="blue">'Usuarios'</span>=>$this->daoUsuarios->findAll()]);
-                    <span class="blue">$this-></span>view->page_title = 'Listar';
-                    <span class="blue">$this-></span>view->render();
+                    $this->view('Listar',[<span class="blue">'id'</span> =>$id, <span class="blue">'name'</span>=>$name,<span class="blue">'Usuarios'</span>=>$this->daoUsuarios->findAll()]);
+                    $this->view->page_title = 'Listar';
+                    $this->view->render();
                 }
             }
             </code></pre></figure>
